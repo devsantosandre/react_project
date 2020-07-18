08b0d1389751746519abb7f1ee0e9b5f28f939d6
André Santos <dev.santosandre@gmail.com>
2020-07-18 00:04:57 -0300

    UPDATE - funcando dados do site

--- aplicacao/react_project/src/todo/todo.jsx
+++ aplicacao/react_project/src/todo/todo.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://ws.audioscrobbler.com/2.0/'
+
 export default class Todo extends Component {
-    render(){
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        // this.handleAdd = this.handleAdd.bind(this)
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?method=artist.getsimilar&artist=bonjovi&api_key=53701585b00198db3a80dcf69bc4ef8c&format=json`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
-                <TodoList />
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
+                <TodoList list={this.state.list}/>
             </div>
         )
     }
--- aplicacao/react_project/src/todo/todoList.jsx
+++ aplicacao/react_project/src/todo/todoList.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-export default props => (
-    <div>
-        <h1>Lista</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return  (
+            console.log(props.list)
+        )
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+
+        </table>
+    )
+}
